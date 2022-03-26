9f5737e70d72c692b52e6eeb5d4a997e1f0533b4
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-03-26 11:33:46 -0400

    creating skel

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,8 @@
 
 kubectl delete -f deployment.yaml --namespace r-mordasiewicz
 
+
 kubectl create -f deployment.yaml --namespace r-mordasiewicz
+
+
+echo "kubectl exec --namespace r-mordasiewicz -it ubuntu -c ubuntu -- /bin/bash"
