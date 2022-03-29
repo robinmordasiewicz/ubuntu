8a4787167b407c81e02d3c6ad1c20e4c0fac5497
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-03-29 13:52:40 -0400

    creating skel

--- release.sh
+++ release.sh
@@ -15,6 +15,9 @@ IMAGE=ubuntu
 cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION.tmp && mv VERSION.tmp VERSION
 version=`cat VERSION`
 echo "version: $version"
+
+sed -e "s/[0-9].[0-9].[0-9]/${version}/" deployment.yaml > deployment.yaml.tmp && mv deployment.yaml.tmp deployment.yaml
+
 # run build
 ./build.sh
 # tag it
